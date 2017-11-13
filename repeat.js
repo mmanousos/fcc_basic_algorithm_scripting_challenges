89767384e9810a4803c5512d34d98e87226428b4
M. Manousos <mmanousos@gmail.com>
2017-11-13 14:46:21 -0800

    final solution

--- repeat.js
+++ repeat.js
@@ -1,19 +1,23 @@
 function repeatStringNumTimes(str, num) {
-    var newArr = [];
-        console.log
-    if (num < 0) {
-        newArr.push("");
-        return newArr.toString();
+    var newArr = []; // create empty array
+    if (num < 0) { // check if "num" is negative
+        newArr.push(""); // if it is, push empty string to "newArr"
+        return newArr.toString(); // return empty string
+    } else { // otherwise
+        while ( num > 0 ) {  // loop through value of "num"
+            newArr.push(str); // add "str" to array
+            num--; // decrease "num" until 0
+        } return newArr.join(""); // return contents of array as joined string
     }
 }
+
+repeatStringNumTimes("abc", 3);
 
 // create new array
 // check if "num" is a positive #
     // if no, return empty string from array
     // if yes, push str to array & reduce num by 1 to repeat. 
     
-
-repeatStringNumTimes("abc", 3);
 
 
 /* Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive number.
