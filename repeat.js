60423d49014dae58867cc39c83dd48caa8069561
M. Manousos <mmanousos@gmail.com>
2017-11-13 14:12:19 -0800

    pseudocode

--- repeat.js
+++ repeat.js
@@ -1,3 +1,17 @@
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  return str;
+}
+
+// create new array
+// check if "num" is a positive #
+    // if no, return empty string from array
+    // if yes, push str to array & reduce num by 1 to repeat. 
+    
+
+repeatStringNumTimes("abc", 3);
+
+
 /* Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive number.
 
 // repeatStringNumTimes("*", 3) should return "***".
