c95e1a7d629f0aff7ff952a50aae07e99fc4d53c
M. Manousos <mmanousos@gmail.com>
2017-11-13 14:09:23 -0800

    assignment

---
+++ repeat.js
@@ -0,0 +1,8 @@
+/* Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive number.
+
+// repeatStringNumTimes("*", 3) should return "***".
+// repeatStringNumTimes("abc", 3) should return "abcabcabc".
+// repeatStringNumTimes("abc", 4) should return "abcabcabcabc".
+// repeatStringNumTimes("abc", 1) should return "abc".
+// repeatStringNumTimes("*", 8) should return "********".
+// repeatStringNumTimes("abc", -2) should return "".
