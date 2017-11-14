cbf99d94c7e71e2eaf9cad1c95c37a08ebef9a88
M. Manousos <mmanousos@gmail.com>
2017-11-14 11:19:21 -0800

    final solution with alt that doesn't meet all reqs

--- truncate.js
+++ truncate.js
@@ -1,19 +1,41 @@
+/* Final solution that works for additional, longer "num" arguments */ 
+
 function truncateString(str, num) {
     var i = str.length;
     if (i > num) {
-        var j = str.substring(num-1);
-            console.log(j);
-        
-        // var j = Array.from(str);
-        // var k = j.slice();  
-        // var newStr = k.substr(num-1); 
-        //    console.log(newStr);
-        // var k = j.substring(11);
-    }
-  // return newStr;
+        if (num <= 3) {
+           var j = str.substring(0,num);
+           var newStr = (j + "...");
+            return newStr;
+        } else {
+            var k = str.substring(0,num-3);
+            var altNewStr = (k + "...");
+            return altNewStr;
+        }
+    } return str;
 }
 
-truncateString("A-tisket a-tasket A green and yellow basket", 11);
+truncateString("Absolutely Longer", 2);
+
+
+/* Solution works for numerical "num" arguments shorter than "str" but doesn't account for longer options */
+
+function truncateString(str, num) {
+    var i = str.length;
+    if (i > num) {
+        if (num <= 3) {
+           var j = str.substring(0,num);
+            console.log(j);  
+        } else {
+            var j = str.substring(0,num-3);
+            console.log(k);    
+        } var newStr = (j + "...");
+    } return newStr;
+}
+
+truncateString("Absolutely Longer", 2);
+
+/* -- PSEUDOCODE -- */
 
 // get length of string
 // check against "num" 
@@ -28,6 +50,8 @@ truncateString("A-tisket a-tasket A green and yellow basket", 11);
 // return new substring
 
 
+
+/* -- ASSIGNMENT -- */
 /* Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
 
 Note that inserting the three dots to the end will add to the string length.
