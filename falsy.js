d74194c5554f5d047756812e2b9239a4f6436d40
M. Manousos <mmanousos@gmail.com>
2017-11-15 10:25:27 -0800

    pseudocode

--- falsy.js
+++ falsy.js
@@ -7,6 +7,11 @@ function bouncer(arr) {
 bouncer([7, "ate", "", false, 9]);
 
 
+/* -- PSEUDOCODE -- */
+
+// filter array checking if elements of array are false
+
+
 /* -- ASSIGNMENT -- */ 
 
 /* Remove all falsy values from an array.
@@ -25,3 +30,16 @@ bouncer(["a", "b", "c"]) should return ["a", "b", "c"].
 bouncer([false, null, 0, NaN, undefined, ""]) should return [].
 bouncer([1, null, NaN, 2, undefined]) should return [1, 2].
 */
+
+/* info about ".filter()" method
+Returns a new array, does not modify the original source. New array contains only those elements for which the callback (argument) method that we pass in return true.
+Takes a function as an argument with up to three (optional) inputs:
+An element in the array
+The index of this element in the array
+The array itself
+e.g. var arr = [1,2,3,4];
+var arrTransformed = arr.filter(function(element/val, index, arr) {
+  return index % 2; 
+}); 
+console.log(arrTransformed); // [2, 4]
+// only the values divisible by 2 are returned in the new array
